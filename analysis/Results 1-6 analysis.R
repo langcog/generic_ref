df22fe6a7c7486979eb10298b53031b6304e6d46
Phil Crone <philip.crone@gmail.com>
2015-01-06 16:41:24 -0800

    New data/analysis
    New data and analysis from experiment run on 1/6/2015. Each participant
    saw 24 sentences, corresponding to those sentences produced during the
    11/17/2014 experiment by a single Turker when images matched subject in
    number. On the new experiment, images were split 50-50 between matching
    and mismatching in number.

---
+++ analysis/Results 1-6 analysis.R
@@ -0,0 +1,96 @@
+rm(list=ls())
+source("~/Projects/R/Ranalysis/useful.R") # from github.com/langcog/Ranalysis
+d = read.csv("data/Results 1-6-parsed.csv")
+
+colnames(d)[3] = 'language'
+colnames(d)[4] = 'feedback'
+
+### DATA CLEANUP
+d = d[d$language != '\"Italian\"' & d$language != '\"korean\"' & d$language != '\"Punjabi\"' & d$language != '\"Russian\"' & d$language != '\"Spanish\"',]
+levels(d$language) = droplevels(d$language)
+
+d = d[d$sentence_id != 1177 & d$sentence_id != 2238 & d$sentence_id != 641,] # Three Turker-generated sentences were removed because they were possibly offensive. They were replaced with dummy sentences using the same subject. 
+
+targets = subset(d,type=='target')
+
+mean.rt= mean(targets$rt)
+sd.rt = sd(targets$rt)
+targets = subset(targets, (targets$rt - mean.rt) < abs(2 * sd.rt))
+
+### HISTOGRAMS
+qplot(response, facets = definiteness ~ number, data=targets)
+qplot(response, facets = definiteness ~ animacy, data=targets)
+
+### PLOT WITHOUT ANIMACY
+mss <- ddply(targets, .(definiteness,number,WorkerId), summarise,
+             response = mean(response))
+
+ms <- ddply(mss, .(definiteness,number), summarise,
+            mean = mean(response),
+            cil = ci.low(response),
+            cih = ci.high(response))
+
+qplot(definiteness, mean, fill=number, 
+      group=number, stat="identity",
+      position=position_dodge(width=.9),
+      ymin=mean - cil, ymax=mean + cih,
+      geom=c("bar","linerange"),data=ms)
+
+### NOW WITH ANIMACY
+
+mssa <- ddply(targets, .(definiteness,number,animacy,WorkerId), summarise,
+              response = mean(response))
+
+msa <- ddply(mssa, .(definiteness,number,animacy), summarise,
+             mean = mean(response),
+             cil = ci.low(response),
+             cih = ci.high(response))
+
+qplot(definiteness, mean, fill=number, 
+      facets=.~animacy,
+      group=number, stat="identity",
+      position=position_dodge(width=.9),
+      ymin=mean - cil, ymax=mean + cih,
+      geom=c("bar","linerange"),data=msa)
+
+### NOW WITH IMAGE AND WITHOUT ANIMACY
+mssa <- ddply(targets, .(definiteness,number,image,WorkerId), summarise,
+              response = mean(response))
+
+msa <- ddply(mssa, .(definiteness,number,image), summarise,
+             mean = mean(response),
+             cil = ci.low(response),
+             cih = ci.high(response))
+
+qplot(definiteness, mean, fill=number, 
+      facets=.~image,
+      group=number, stat="identity",
+      position=position_dodge(width=.9),
+      ymin=mean - cil, ymax=mean + cih,
+      geom=c("bar","linerange"),data=msa)
+
+### NOW WITH IMAGE AND ANIMACY
+mssa <- ddply(targets, .(definiteness,number,image,animacy,WorkerId), summarise,
+              response = mean(response))
+
+msa <- ddply(mssa, .(definiteness,number,image,animacy), summarise,
+             mean = mean(response),
+             cil = ci.low(response),
+             cih = ci.high(response))
+
+qplot(definiteness, mean, fill=number, 
+      facets=animacy~image,
+      group=number, stat="identity",
+      position=position_dodge(width=.9),
+      ymin=mean - cil, ymax=mean + cih,
+      geom=c("bar","linerange"),data=msa)
+
+### MODEL WE SHOULD BE FITTING
+mod <- lmer(response ~ animacy * definiteness * number + 
+              (animacy * definiteness * number | WorkerId) + 
+              (definiteness * number | subject), data=targets)
+
+### FASTER MODEL
+mod <- lmer(response ~ animacy * definiteness * number * image + 
+              (animacy + definiteness + number  | WorkerId) + 
+              (definiteness + number | subject), data=targets)
